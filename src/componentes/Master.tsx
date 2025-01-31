ac14815f8a8f3a1af8d6a31b713ecb31a6f29e5b
xavikortes <javiercortesveliz@gmail.com>
2025-01-31 14:53:59 +0100

    feat. pruebas crear, actualizar, borrar

--- src/componentes/Master.tsx
+++ src/componentes/Master.tsx
@@ -16,9 +16,55 @@ type MasterProps<T> = {
 };
 
 export const Master = <T extends Entidad>({ acciones }: MasterProps<T>) => {
-  const { obtenerTodos } = acciones;
+  const { obtenerTodos, crearUno, actualizarUno } = acciones;
 
   const [entidades, setEntidades] = useState<T[]>([]);
+
+  const onCrearNuevo = () => {
+    const nuevaEntidad = {
+      id: (entidades.length + 1).toString().padStart(6, "0"),
+      nombre: [
+        "Michel Jordan",
+        "Lebron James",
+        "Kobe Bryant",
+        "Stephen Curry",
+        "Kevin Durant",
+        "James Harden",
+        "Anthony Davis",
+        "Giannis Antetokounmpo",
+        "Luka Doncic",
+        "Damian Lillard",
+      ][entidades.length % 10],
+      id_fiscal: "53631867F",
+    } as unknown as T;
+
+    crearUno(nuevaEntidad).then(() => {
+      setEntidades([nuevaEntidad, ...entidades]);
+    });
+  };
+
+  const onActualizarPrimero = () => {
+    const primeraEntidad = entidades[0];
+
+    const nuevaEntidad = {
+      id: primeraEntidad.id,
+      nombre: primeraEntidad.nombre + " (actualizado)",
+      nada: "",
+    } as unknown as Partial<T>;
+
+    actualizarUno(nuevaEntidad).then(() => {
+      setEntidades([
+        { ...primeraEntidad, nombre: nuevaEntidad.nombre },
+        ...entidades.slice(1),
+      ]);
+    });
+  };
+
+  const onEliminarPrimero = () => {
+    acciones.eliminarUno(entidades[0].id).then(() => {
+      setEntidades(entidades.slice(1));
+    });
+  };
 
   useEffect(() => {
     obtenerTodos().then((entidades) => setEntidades(entidades as T[]));
@@ -26,6 +72,9 @@ export const Master = <T extends Entidad>({ acciones }: MasterProps<T>) => {
 
   return (
     <ul>
+      <button onClick={onCrearNuevo}>Crear nuevo</button>
+      <button onClick={onActualizarPrimero}>Actualizar primero</button>
+      <button onClick={onEliminarPrimero}>Eliminar primero</button>
       {entidades.map((entidad) => {
         const { id, ...resto } = entidad;
         return (
