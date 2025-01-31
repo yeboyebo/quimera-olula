74484df32298151c861acb024cd9f41e39babca6
xavikortes <javiercortesveliz@gmail.com>
2025-01-31 13:14:48 +0100

    feat. ficheros rest y fake data

---
+++ src/contextos/ventas/clientes/clientesFake.ts
@@ -0,0 +1,69 @@
+import { Cliente } from "./Clientes.tsx";
+
+export const clientesFake: Cliente[] = [
+  {
+    id: "000001",
+    nombre: "Álvaro Micó",
+    id_fiscal: "12595882R",
+  },
+  {
+    id: "000002",
+    nombre: "Óscar Lorenzo",
+    id_fiscal: "70499129N",
+  },
+  {
+    id: "000003",
+    nombre: "José Alberola",
+    id_fiscal: "68350847E",
+  },
+  {
+    id: "000004",
+    nombre: "Samuel Guardiola",
+    id_fiscal: "84254150K",
+  },
+  {
+    id: "000005",
+    nombre: "Miguel Ráez",
+    id_fiscal: "83690496M",
+  },
+  {
+    id: "000006",
+    nombre: "Rubén Naranjo",
+    id_fiscal: "53631867F",
+  },
+  {
+    id: "000007",
+    nombre: "Silfriel Anthony Vega",
+    id_fiscal: "28421342N",
+  },
+  {
+    id: "000008",
+    nombre: "Pablo Rosique",
+    id_fiscal: "64350179C",
+  },
+  {
+    id: "000009",
+    nombre: "Luis Francisco Sánchez",
+    id_fiscal: "47040997V",
+  },
+  {
+    id: "000010",
+    nombre: "Francisco José Martínez",
+    id_fiscal: "16440718L",
+  },
+  {
+    id: "000011",
+    nombre: "Papa Ndiassé",
+    id_fiscal: "85564570Q",
+  },
+  {
+    id: "000012",
+    nombre: "Ángel Martínez",
+    id_fiscal: "36931218A",
+  },
+  {
+    id: "000013",
+    nombre: "Mario Pérez",
+    id_fiscal: "00055373N",
+  },
+];
