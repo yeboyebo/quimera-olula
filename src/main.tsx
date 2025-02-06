98a6482343c4b1dae69bc23009f314df5535282b
xavikortes <javiercortesveliz@gmail.com>
2025-02-06 16:38:30 +0100

    feat. Indice y rutas reestructuracion

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { Clientes } from "./contextos/ventas/clientes/Clientes.tsx";
-import { Cliente } from "./contextos/ventas/cliente/Cliente.tsx";
+import { Indice } from "./contextos/comun/Indice.tsx";
+import { DetalleCliente } from "./contextos/ventas/cliente/vistas/DetalleCliente.tsx";
+import { MaestroCliente } from "./contextos/ventas/cliente/vistas/MaestroCliente.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<Clientes />} />
-        <Route path="cliente/:id" element={<Cliente />} />
+        <Route index element={<Indice />} />
+        <Route path="ventas">
+          <Route path="cliente">
+            <Route index element={<MaestroCliente />} />
+            <Route path=":id" element={<DetalleCliente />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
