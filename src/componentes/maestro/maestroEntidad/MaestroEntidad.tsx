ba36ab83fdcb0f8282564df06b85a27940ecaeee
xavikortes <javiercortesveliz@gmail.com>
2025-02-10 12:35:13 +0100

    feat. entidades en Maestro muestran campos anidados (1 nivel)

--- src/componentes/maestro/maestroEntidad/MaestroEntidad.tsx
+++ src/componentes/maestro/maestroEntidad/MaestroEntidad.tsx
@@ -11,12 +11,22 @@ export const MaestroEntidad = <T extends Entidad>({
 
   return (
     <li key={id} style={{ display: "flex", flexDirection: "column" }}>
-      <span>Cliente: {id}</span>
-      {Object.entries(resto).map(([clave, valor]) => (
-        <span style={{ marginLeft: "1rem" }} key={clave}>
-          {clave}: {valor}
-        </span>
-      ))}
+      <span>ID: {id}</span>
+      {Object.entries(resto)
+        .filter(([, valor]) => !Array.isArray(valor))
+        .flatMap(([clave, valor]) => {
+          if (valor.constructor !== Object) return [[clave, valor]];
+
+          return Object.entries(valor).map(([claveInterna, valor]) => [
+            clave + "." + claveInterna,
+            valor,
+          ]);
+        })
+        .map(([clave, valor]) => (
+          <span key={clave} style={{ marginLeft: "1rem" }}>
+            {clave}: {valor}
+          </span>
+        ))}
     </li>
   );
 };
