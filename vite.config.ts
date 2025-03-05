9422878fa8a7c24fc87be9934784ff7fa9957a9f
xavikortes <javiercortesveliz@gmail.com>
2025-03-05 11:40:10 +0100

    feat. importacion y declaracion de webcomps

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "quimera-boton": "https://cdn.jsdelivr.net/gh/yeboyebo/quimera-componentes@main/src/atomos/quimera-boton/quimera-boton.min.js",
+      "quimera-input": "https://cdn.jsdelivr.net/gh/yeboyebo/quimera-componentes@main/src/atomos/quimera-input/quimera-input.min.js",
+    },
+  }
 });
