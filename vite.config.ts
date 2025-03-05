a2a69ed24ae35c8a29d12054265eaf25edc5bb69
Iván Vizcaíno <ivan@yeboyebo.es>
2025-03-05 14:24:55 +0000

    Configuracion basica PWA Cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,46 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ERP Web',
+        short_name: 'ERP',
+        description: 'Quimera Olula ERP developed by YEBOYEBO',
+        theme_color: '#ffffff',
+      },
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        runtimeCaching: [
+          {
+            urlPattern: new RegExp("^http://localhost:8005/cache"),
+            handler: "CacheFirst",
+            method: "GET",
+            options: {
+              cacheName: "cache-erp",
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 2, // <== 2 days
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    })
+  ],
 });
