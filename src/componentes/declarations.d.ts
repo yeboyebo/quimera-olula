ebbd28e70e11d0c62a59b6cda1e03c9dde5d91a3
xavikortes <javiercortesveliz@gmail.com>
2025-03-06 10:37:31 +0100

    fix. mejora tipos webcomps

--- src/componentes/declarations.d.ts
+++ src/componentes/declarations.d.ts
@@ -1,29 +1,39 @@
 declare module 'quimera-boton';
 declare module 'quimera-input';
 
+interface NodoComun {
+    id?: string;
+    children?: ReactNode;
+}
+
+interface QuimeraBoton extends NodoComun {
+    tipo?: string;
+    variante?: "solido" | "borde" | "texto";
+    tamaño?: "pequeño" | "mediano" | "grande";
+    destructivo?: boolean;
+    deshabilitado?: boolean;
+    onClick?: (e: MouseEvent) => void;
+}
+
+interface QuimeraInput extends NodoComun {
+    nombre: string;
+    label: string;
+    placeholder?: string;
+    valor?: string;
+    erroneo?: boolean;
+    advertido?: boolean;
+    valido?: boolean;
+    opcional?: boolean;
+    deshabilitado?: boolean;
+    "texto-validacion"?: string;
+    onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
 declare namespace React {
     namespace JSX {
         interface IntrinsicElements {
-            'quimera-boton': {
-                children?: ReactNode;
-                tipo?: string;
-                variante?: "solido" | "borde" | "texto";
-                tamaño?: "pequeño" | "mediano" | "grande";
-                destructivo?: boolean;
-                deshabilitado?: boolean;
-            };
-            'quimera-input': {
-                nombre: string;
-                label: string;
-                placeholder?: string;
-                valor?: string;
-                erroneo?: boolean;
-                advertido?: boolean;
-                valido?: boolean;
-                opcional?: boolean;
-                deshabilitado?: boolean;
-                "texto-validacion"?: string;
-            };
+            'quimera-boton': QuimeraBoton;
+            'quimera-input': QuimeraInput;
         }
     }
 }
