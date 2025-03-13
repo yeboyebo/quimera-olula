a2e22ea4246a49df42a73063b85ff15c895c308e
xavikortes <javiercortesveliz@gmail.com>
2025-03-13 14:08:01 +0100

    feat. cambiar maestro por componente tabla

--- src/componentes/maestro/Maestro.tsx
+++ src/componentes/maestro/Maestro.tsx
@@ -3,9 +3,9 @@ import { Contexto } from "../../contextos/comun/contexto.ts";
 import { Acciones, Entidad } from "../../contextos/comun/diseño.ts";
 import { CampoFormularioGenerico } from "../detalle/FormularioGenerico.tsx";
 import { SinDatos } from "../SinDatos/SinDatos.tsx";
+import { Tabla } from "../wrappers/tabla.tsx";
 import { MaestroAcciones } from "./maestroAcciones/MaestroAcciones.tsx";
 import { MaestroCargando } from "./maestroCargando/MaestroCargando.tsx";
-import { MaestroEntidad } from "./maestroEntidad/MaestroEntidad.tsx";
 import { MaestroFiltros } from "./maestroFiltros/MaestroFiltros.tsx";
 
 /* eslint-disable  @typescript-eslint/no-explicit-any */
@@ -27,7 +27,7 @@ export const Maestro = <T extends Entidad>({
   if (!context) {
     throw new Error("Contexto nulo");
   }
-  const { entidades, setEntidades, setSeleccionada } = context;
+  const { entidades, setEntidades, seleccionada, setSeleccionada } = context;
 
   useEffect(() => {
     obtenerTodos().then((entidades) => {
@@ -36,64 +36,16 @@ export const Maestro = <T extends Entidad>({
     });
   }, [obtenerTodos, setEntidades]);
 
-  const renderCabecera = () => {
-    const { id, ...resto } = entidades[0];
-    return (
-      <li className="CabeceraMaestroEntidad">
-        <span className="id" data-id={id}>
-          ID
-        </span>
-        {Object.entries(resto)
-          .filter(([, valor]) => !Array.isArray(valor))
-          .flatMap(([clave, valor]) => {
-            if (valor?.constructor !== Object) return [[clave, valor]];
-
-            return Object.entries(valor).map(([claveInterna, valor]) => [
-              claveInterna,
-              valor,
-            ]);
-          })
-          .map(([clave]) => (
-            <span key={clave} style={{ marginLeft: "1rem" }}>
-              {clave}
-            </span>
-          ))}
-      </li>
-    );
-  };
-
   const renderEntidades = () => {
-    if (isLoading) {
-      return <MaestroCargando />;
-    }
-
-    if (entidades.length === 0) {
-      return <SinDatos />;
-    }
+    if (isLoading) return <MaestroCargando />;
+    if (entidades.length === 0) return <SinDatos />;
 
     return (
-      <ul
-        className="MaestroEntidades"
-        style={{
-          width: "100%",
-          overflow: "auto",
-          maxHeight: "70vh",
-          flexDirection: "column",
-          display: "flex",
-        }}
-      >
-        {renderCabecera()}
-        {entidades.map((entidad) => {
-          const { id } = entidad;
-          return (
-            <MaestroEntidad
-              key={id}
-              entidad={entidad}
-              onClick={setSeleccionada}
-            />
-          );
-        })}
-      </ul>
+      <Tabla
+        datos={entidades}
+        seleccionadaId={seleccionada?.id}
+        onSeleccion={setSeleccionada}
+      />
     );
   };
 
