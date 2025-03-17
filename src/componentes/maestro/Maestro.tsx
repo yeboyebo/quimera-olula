4e8b5d337cc98fa9d2e5f1585332ddcb08ea87eb
xavikortes <javiercortesveliz@gmail.com>
2025-03-17 12:15:23 +0100

    feat. filtro mejorado

--- src/componentes/maestro/Maestro.tsx
+++ src/componentes/maestro/Maestro.tsx
@@ -22,6 +22,9 @@ export const Maestro = <T extends Entidad>({
 }: MaestroProps<T>) => {
   const { obtenerTodos } = acciones;
   const [isLoading, setIsLoading] = useState(true);
+  const [filtro, setFiltro] = useState<{ [campo: string]: string } | null>(
+    null
+  );
 
   const context = useContext(Contexto);
   if (!context) {
@@ -36,13 +39,30 @@ export const Maestro = <T extends Entidad>({
     });
   }, [obtenerTodos, setEntidades]);
 
+  const entidadesFiltradas = entidades.filter((entidad) => {
+    if (!filtro) return true;
+
+    return Object.entries(filtro).every(([campo, valor]) => {
+      if (!campo.includes(".")) {
+        return (entidad[campo] as string)
+          .toLowerCase()
+          .includes(valor.toLocaleLowerCase());
+      }
+
+      const [clave, claveInterna] = campo.split(".");
+      return (entidad[clave] as Record<string, string>)[claveInterna]
+        .toLocaleLowerCase()
+        .includes(valor.toLocaleLowerCase());
+    });
+  });
+
   const renderEntidades = () => {
     if (isLoading) return <MaestroCargando />;
-    if (entidades.length === 0) return <SinDatos />;
+    if (entidadesFiltradas.length === 0) return <SinDatos />;
 
     return (
       <Tabla
-        datos={entidades}
+        datos={entidadesFiltradas}
         seleccionadaId={seleccionada?.id}
         onSeleccion={setSeleccionada}
       />
@@ -56,7 +76,7 @@ export const Maestro = <T extends Entidad>({
       ) : (
         <MaestroAcciones acciones={acciones} camposEntidad={camposEntidad} />
       )}
-      <MaestroFiltros acciones={acciones} />
+      <MaestroFiltros acciones={acciones} setFiltro={setFiltro} />
       {renderEntidades()}
     </div>
   );
--- src/componentes/maestro/maestroFiltros/MaestroFiltros.tsx
+++ src/componentes/maestro/maestroFiltros/MaestroFiltros.tsx
@@ -1,84 +1,100 @@
-import { useContext, useEffect, useState } from "react";
+import { FormEvent, useContext } from "react";
 import { Contexto } from "../../../contextos/comun/contexto.ts";
 import { Acciones, Entidad } from "../../../contextos/comun/diseño.ts";
+import { CampoFormularioGenerico } from "../../detalle/FormularioGenerico.tsx";
+import {
+  expandirEntidad,
+  formatearClave,
+  renderInput,
+  renderSelect,
+} from "../../detalle/helpers.tsx";
 import "./MaestroFiltros.css";
 
 type MaestroProps<T extends Entidad> = {
   acciones: Acciones<T>;
+  setFiltro: (filtro: { [campo: string]: string } | null) => void;
 };
 
 export const MaestroFiltros = <T extends Entidad>({
   acciones,
+  setFiltro,
 }: MaestroProps<T>) => {
   const { buscar } = acciones;
-  const [originalEntidades, setOriginalEntidades] = useState<T[]>([]);
+
   const context = useContext(Contexto);
   if (!context) {
     throw new Error("Contexto is null");
   }
+
   const { entidades, setEntidades } = context;
 
-  useEffect(() => {
-    if (originalEntidades.length < entidades.length) {
-      setOriginalEntidades(entidades as T[]);
-    }
-  }, [entidades, originalEntidades]);
+  const onBuscar = (event: FormEvent): void => {
+    event.preventDefault();
 
-  const onBuscar = (formData: FormData): void => {
+    const formData = new FormData(event.target as HTMLFormElement);
     const campo = formData.get("campo") as string;
     const valor = formData.get("valor") as string;
-    if (buscar !== undefined) {
-      // Busco en el servidor
-      buscar(campo, valor).then((entidades) => setEntidades(entidades as T[]));
+
+    if (!buscar) {
+      setFiltro({ [campo]: valor });
       return;
     }
-    // Busco de forma local
-    const entidadesFiltradas = entidades.filter((entidad) => {
-      if (!campo.includes(".")) {
-        return (entidad[campo] as string).includes(valor);
-      }
 
-      const [clave, claveInterna] = campo.split(".");
-      return (entidad[clave] as Record<string, string>)[claveInterna].includes(
-        valor
-      );
-    });
-    setEntidades(entidadesFiltradas);
+    buscar(campo, valor).then((entidades) => setEntidades(entidades as T[]));
   };
 
   const onLimpiar = () => {
-    setEntidades(originalEntidades);
+    setFiltro(null);
   };
 
   const obtenerCampos = () => {
-    if (originalEntidades.length === 0) {
-      return [];
-    }
+    const entidad = entidades[0];
+    if (!entidad) return [];
 
-    return Object.entries(originalEntidades[0]).flatMap(([clave, valor]) => {
-      if (valor?.constructor !== Object) return [clave];
+    const entidadExpandida = expandirEntidad(entidad);
 
-      return Object.keys(valor).map(
-        (claveInterna) => clave + "." + claveInterna
-      );
-    });
+    return entidadExpandida.map(([clave]) => ({
+      campo: clave,
+      descripcion: formatearClave(clave),
+    }));
+  };
+
+  const selectorCampo = () => {
+    const attrsCampo: CampoFormularioGenerico = {
+      nombre: "campo",
+      etiqueta: "Filtro",
+      tipo: "select",
+      requerido: true,
+      opciones: obtenerCampos(),
+      condensado: true,
+    };
+
+    return renderSelect(attrsCampo, {} as Entidad);
+  };
+
+  const inputFiltro = () => {
+    const attrsValor: CampoFormularioGenerico = {
+      nombre: "valor",
+      etiqueta: "&nbsp;",
+      placeholder: "Valor a filtrar",
+      tipo: "text",
+      requerido: true,
+      condensado: true,
+    };
+    return renderInput(attrsValor, {} as Entidad);
   };
 
   return (
     <div className="MaestroFiltros">
-      <form action={onBuscar}>
-        <select name="campo">
-          {obtenerCampos().map((campo) => (
-            <option key={campo} value={campo}>
-              {campo}
-            </option>
-          ))}
-        </select>
-        <input type="text" name="valor" placeholder="Valor" />
-        <button>Buscar</button>
-      </form>
-      <form action={onLimpiar}>
-        <button>Limpiar</button>
+      <form onSubmit={onBuscar} onReset={onLimpiar}>
+        {selectorCampo()}
+        {inputFiltro()}
+        <quimera-boton tipo="submit" tamaño="pequeño">
+          Buscar
+        </quimera-boton>
+        <quimera-boton tipo="reset" variante="texto" tamaño="pequeño">
+          Limpiar
+        </quimera-boton>
       </form>
     </div>
   );
