55d785aa664f3edc90b58af6d0d18d66e6e07206
xavikortes <javiercortesveliz@gmail.com>
2025-03-17 09:51:44 +0100

    fix. sacando funciones comunes del componente formulario generico

--- src/componentes/detalle/FormularioGenerico.tsx
+++ src/componentes/detalle/FormularioGenerico.tsx
@@ -2,6 +2,45 @@ import React from "react";
 import { Entidad } from "../../contextos/comun/diseño.ts";
 import { Select } from "../wrappers/select.tsx";
 
+const renderSelect = (campo: CampoFormularioGenerico, entidad: Entidad) => {
+  const attrs = {
+    nombre: campo.nombre,
+    label: campo.etiqueta,
+    placeholder: `Selecciona un/a ${campo.etiqueta.toLowerCase()}`,
+    opcional: !campo.requerido,
+    deshabilitado: campo.soloLectura,
+    valor: entidad[campo.nombre] as string,
+    opciones: campo.opciones?.map((opcion) => ({
+      valor: opcion[campo.nombre],
+      descripcion: opcion["descripcion"],
+    })),
+    "todo-ancho": campo.ancho === "100%" ? "true" : undefined,
+  };
+
+  return <Select key={campo.nombre} {...attrs} />;
+};
+
+const renderInput = (campo: CampoFormularioGenerico, entidad: Entidad) => {
+  const attrs = {
+    nombre: campo.nombre,
+    label: campo.etiqueta,
+    placeholder: `Introduce el valor de ${campo.etiqueta.toLowerCase()}`,
+    valor: entidad[campo.nombre] as string,
+    opcional: !campo.requerido,
+    deshabilitado: campo.soloLectura,
+    "todo-ancho": campo.ancho === "100%" ? "true" : undefined,
+  };
+  return (
+    <React.Fragment key={campo.nombre}>
+      <quimera-input {...attrs}></quimera-input>
+    </React.Fragment>
+  );
+};
+
+const renderSpace = () => {
+  return <div key="space" style={{ height: "1rem", width: "100%" }}></div>;
+};
+
 export type CampoFormularioGenerico = {
   nombre: string;
   etiqueta: string;
@@ -48,45 +87,6 @@ export const FormularioGenerico = <T extends Entidad>({
     return <>No encontrado</>;
   }
 
-  const renderSelect = (campo: CampoFormularioGenerico) => {
-    const attrs = {
-      nombre: campo.nombre,
-      label: campo.etiqueta,
-      placeholder: `Selecciona un/a ${campo.etiqueta.toLowerCase()}`,
-      opcional: !campo.requerido,
-      deshabilitado: campo.soloLectura,
-      valor: entidad[campo.nombre] as string,
-      opciones: campo.opciones?.map((opcion) => ({
-        valor: opcion[campo.nombre],
-        descripcion: opcion["descripcion"],
-      })),
-      "todo-ancho": campo.ancho === "100%" ? "true" : undefined,
-    };
-
-    return <Select key={campo.nombre} {...attrs} />;
-  };
-
-  const renderInput = (campo: CampoFormularioGenerico) => {
-    const attrs = {
-      nombre: campo.nombre,
-      label: campo.etiqueta,
-      placeholder: `Introduce el valor de ${campo.etiqueta.toLowerCase()}`,
-      valor: entidad[campo.nombre] as string,
-      opcional: !campo.requerido,
-      deshabilitado: campo.soloLectura,
-      "todo-ancho": campo.ancho === "100%" ? "true" : undefined,
-    };
-    return (
-      <React.Fragment key={campo.nombre}>
-        <quimera-input {...attrs}></quimera-input>
-      </React.Fragment>
-    );
-  };
-
-  const renderSpace = () => {
-    return <div key="space" style={{ height: "1rem", width: "100%" }}></div>;
-  };
-
   return (
     <form action={handleAction}>
       {campos
@@ -95,8 +95,8 @@ export const FormularioGenerico = <T extends Entidad>({
           campo.tipo === "space"
             ? renderSpace()
             : campo.tipo === "select"
-            ? renderSelect(campo)
-            : renderInput(campo)
+            ? renderSelect(campo, entidad)
+            : renderInput(campo, entidad)
         )}
       <quimera-boton tipo="submit">Enviar</quimera-boton>
     </form>
