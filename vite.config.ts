d0d21a38a0b93bfeb37c246f2dc22282d2e09af6
xavikortes <javiercortesveliz@gmail.com>
2025-04-04 09:48:17 +0200

    fix. eliminar alias componentes externos

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from 'vite-plugin-pwa';
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -43,11 +41,4 @@ export default defineConfig({
       },
     })
   ],
-  resolve: {
-    alias: {
-      "quimera-boton": "https://cdn.jsdelivr.net/gh/yeboyebo/quimera-componentes@main/src/atomos/quimera-boton/quimera-boton.js",
-      "quimera-input": "https://cdn.jsdelivr.net/gh/yeboyebo/quimera-componentes@main/src/atomos/quimera-input/quimera-input.js",
-      "quimera-select": "https://cdn.jsdelivr.net/gh/yeboyebo/quimera-componentes@main/src/atomos/quimera-select/quimera-select.js",
-    },
-  }
 });
