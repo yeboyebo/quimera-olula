c219ef81420509fe169e82f7db45a914fb749fb2
xavikortes <javiercortesveliz@gmail.com>
2025-04-09 15:28:48 +0200

    fix. simplificando y adaptando qcheckbox

--- src/componentes/atomos/qcheckbox.tsx
+++ src/componentes/atomos/qcheckbox.tsx
@@ -1,67 +1,56 @@
+import "./_forminput.css";
+import {
+  Etiqueta,
+  FormInput,
+  FormInputProps,
+  Validacion,
+} from "./_forminput.tsx";
 import "./qcheckbox.css";
 
-type FormInputProps = {
-  label: string;
-  nombre: string;
-  deshabilitado?: boolean;
-  placeholder?: string;
-  valor?: boolean;
-  textoValidacion?: string;
-  erroneo?: boolean;
-  advertido?: boolean;
-  valido?: boolean;
-  opcional?: boolean;
-  condensado?: boolean;
-  onChange?: (valor: boolean, evento: React.ChangeEvent<HTMLElement>) => void;
-  onBlur?: (valor: boolean, evento: React.FocusEvent<HTMLElement>) => void;
-};
-
-type QCheckBoxProps = FormInputProps & {
-  lista?: string;
-  autocompletar?: "off" | "on";
-  onInput?: (valor: string, evento: React.FormEvent<HTMLInputElement>) => void;
+type QCheckBoxProps = Omit<FormInputProps, "valor"> & {
+  valor: boolean;
 };
 
 export const QCheckbox = ({
   label,
   nombre,
   deshabilitado,
-  placeholder,
-  valor = false,
   textoValidacion = "",
+  valor = false,
   erroneo,
   advertido,
   valido,
   opcional,
   condensado,
   onChange,
-  onBlur,
-  onInput,
+  ...props
 }: QCheckBoxProps) => {
-  const attrs = { erroneo, advertido, valido, opcional, condensado };
+  const attrs = {
+    nombre,
+    erroneo,
+    advertido,
+    valido,
+    opcional,
+    condensado,
+    deshabilitado,
+  };
+
+  const inputAttrs = {
+    nombre,
+    deshabilitado,
+    opcional,
+    checked: valor,
+    tipo: "checkbox" as const,
+    onChange,
+    ...props,
+  };
 
   return (
     <quimera-checkbox {...attrs}>
       <label>
-        <span className="etiqueta">
-          {label}&nbsp;
-          <span className="etiqueta-opcional">(opcional)</span>
-        </span>
-        <input
-          type="checkbox"
-          name={nombre}
-          placeholder={placeholder}
-          checked={onChange ? valor : undefined}
-          defaultChecked={onChange ? undefined : valor}
-          disabled={deshabilitado}
-          required={!opcional}
-          onChange={(e) => onChange?.(e.target.checked, e)}
-          onBlur={(e) => onBlur?.(e.target.checked, e)}
-          onInput={(e) => onInput?.((e.target as HTMLInputElement).value, e)}
-        />
-        {textoValidacion && (
-          <span className="texto-validacion">{textoValidacion}</span>
-        )}
+        <FormInput {...inputAttrs} />
+        <Etiqueta label={label} />
+        <Validacion textoValidacion={textoValidacion} />
       </label>
     </quimera-checkbox>
   );
