2880fea0a0a54cef2324f3227a32aee7b8d37300
xavikortes <javiercortesveliz@gmail.com>
2025-04-09 14:45:19 +0200

    feat. simplificando qdate + stories

---
+++ src/componentes/atomos/qdate.stories.tsx
@@ -0,0 +1,45 @@
+import { Historia, MetaHistorias } from "../historias/diseño.tsx";
+import { QDate } from "./qdate.tsx";
+
+export default {
+  grupo: "atomos",
+  titulo: "qdate",
+  attrs: {
+    nombre: "fecha_inicio",
+    label: "Fecha inicio",
+    placeholder: "Introduce la fecha de inicio",
+    textoValidacion: "Preferible a partir de 2026",
+  },
+  Componente: QDate,
+} as unknown as MetaHistorias;
+
+export const Base: Historia = {};
+
+export const Validaciones: Historia = [
+  {
+    textoValidacion: "Debes elegir una fecha",
+    erroneo: "true",
+  },
+  {
+    valor: "2025-10-23",
+    textoValidacion: "Vamos, puedes hacerlo mejor",
+    advertido: "true",
+  },
+  {
+    valor: "2026-04-14",
+    textoValidacion: "Bien! Bonito día",
+    valido: "true",
+  },
+];
+
+export const Opcional: Historia = {
+  opcional: "true",
+};
+
+export const Deshabilitado: Historia = {
+  deshabilitado: "true",
+};
+
+export const Condensado: Historia = {
+  condensado: "true",
+};
--- src/componentes/historias/listado-historias.ts
+++ src/componentes/historias/listado-historias.ts
@@ -1,4 +1,5 @@
 import * as HistoriasBoton from "../atomos/qboton.stories.tsx";
+import * as HistoriasDate from "../atomos/qdate.stories.tsx";
 import * as HistoriasInput from "../atomos/qinput.stories.tsx";
 import * as HistoriasSelect from "../atomos/qselect.stories.tsx";
 import { HistoriasComponente } from "./diseño.tsx";
@@ -7,4 +8,5 @@ export const listadoHistorias = [
     HistoriasBoton,
     HistoriasInput,
     HistoriasSelect,
+    HistoriasDate,
 ] as unknown as HistoriasComponente[];
