546ad0759ef608093bce46c238658d5216ad25c9
xavikortes <javiercortesveliz@gmail.com>
2025-04-09 13:24:46 +0200

    feat. historias componente select

--- src/componentes/historias/listado-historias.ts
+++ src/componentes/historias/listado-historias.ts
@@ -1,8 +1,10 @@
 import * as HistoriasBoton from "../atomos/qboton.stories.tsx";
 import * as HistoriasInput from "../atomos/qinput.stories.tsx";
+import * as HistoriasSelect from "../atomos/qselect.stories.tsx";
 import { HistoriasComponente } from "./diseño.tsx";
 
 export const listadoHistorias = [
     HistoriasBoton,
     HistoriasInput,
+    HistoriasSelect,
 ] as unknown as HistoriasComponente[];
