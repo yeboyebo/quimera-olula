60db37234e24d1623aba1fc499a2fc3bf54202bc
Iván Vizcaíno <ivan@yeboyebo.es>
2025-09-17 08:42:56 +0000

    QuimeraAcciones, diseño e historia de ejemplo

--- src/componentes/historias/listado-historias.ts
+++ src/componentes/historias/listado-historias.ts
@@ -4,6 +4,7 @@ import * as HistoriasDate from "../atomos/qdate.historias.ts";
 import * as HistoriasInput from "../atomos/qinput.historias.ts";
 import * as HistoriasSelect from "../atomos/qselect.historias.ts";
 import * as HistoriasMenuLateral from "../menu/menu-lateral.historias.tsx";
+import * as HistoriasAcciones from "../moleculas/qacciones.historias.ts";
 import * as HistoriasModal from "../moleculas/qmodal.historias.ts";
 import * as HistoriasClientes from "../vista/clientes.historias.tsx";
 import * as HistoriasHome from "../vista/home.historias.tsx";
@@ -16,7 +17,8 @@ export const listadoHistorias = [
     HistoriasDate,
     HistoriasCheckbox,
     HistoriasModal,
+    HistoriasAcciones,
     HistoriasMenuLateral,
     HistoriasHome,
-    HistoriasClientes
+    HistoriasClientes,
 ] as unknown as HistoriasComponente[];
