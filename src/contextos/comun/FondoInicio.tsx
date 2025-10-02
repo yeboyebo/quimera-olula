7c1cdb1df5e94e0df47460c0bc631b89e1500f03
Javier-Valero-yeboyebo <jvaola@gmail.com>
2025-10-02 12:21:50 +0200

    refact: Cambiado texto de cabecera y añadida imagen de fondo.

---
+++ src/contextos/comun/FondoInicio.tsx
@@ -0,0 +1,10 @@
+import "./comun.css";
+
+export const FondoInicio = () => {
+  return (
+    <div className="fondo-inicio">
+      <img src="/logo_olula.png" alt="Logo Olula" className="logo-imagen" />
+      {/* <div className="texto-logo">Olula</div> */}
+    </div>
+  );
+};
