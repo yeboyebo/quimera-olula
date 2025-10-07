1be0b1227420cc9040430776a636d228942512a2
Javier-Valero-yeboyebo <jvaola@gmail.com>
2025-10-07 11:25:01 +0200

    refact: Solo opciones de menu de la porpia apliación.

--- src/apps/almaeventos/factory.ts
+++ src/apps/almaeventos/factory.ts
@@ -1,8 +1,12 @@
-import FactoryOlula from '../olula/factory.ts';
+// import FactoryOlula from '../olula/factory.ts';
 import { FactoryEventosAlma } from './contextos/eventos/factory.ts';
 
 
-export class FactoryAlmaEventos extends FactoryOlula {
+// export class FactoryAlmaEventos extends FactoryOlula {
+//     Eventos = FactoryEventosAlma;
+// }
+
+export class FactoryAlmaEventos {
     Eventos = FactoryEventosAlma;
 }
 
