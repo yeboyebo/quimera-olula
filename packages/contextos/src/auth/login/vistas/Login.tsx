82d6db29fdc52fe5f6ed48b1fd4fcfc7aaab9b61
xavikortes <javiercortesveliz@gmail.com>
2025-11-27 12:27:07 +0100

    feat. comprobar token en middleware y no en api o login

--- packages/contextos/src/auth/login/vistas/Login.tsx
+++ packages/contextos/src/auth/login/vistas/Login.tsx
@@ -3,30 +3,26 @@ import { QCheckbox } from "@olula/componentes/atomos/qcheckbox.tsx";
 import { QForm } from "@olula/componentes/atomos/qform.tsx";
 import { QInput } from "@olula/componentes/atomos/qinput.tsx";
 import { useState } from "react";
-import { comprobarToken, login } from "../dominio.ts";
+import { useNavigate } from "react-router";
+import { login } from "../dominio.ts";
 import { misPermisos, permisosGrupo } from "../infraestructura.ts";
 import estilos from "./Login.module.css";
 
-const MINUTOS = 60 * 1000;
-
-setInterval(() => {
-  comprobarToken()?.catch((_e) => {
-    return;
-  });
-}, 2 * MINUTOS);
-
 export const Login = () => {
+  const navigate = useNavigate();
+
   const [mostrarContraseña, setMostrarContraseña] = useState(false);
 
   const loginSubmit = async (datos: Record<string, string>) => {
     const { id, contraseña } = datos;
 
-    login(id, contraseña).then(() => {
-      misPermisos().then((datosPermisos) => {
-        permisosGrupo.actualizar(datosPermisos.datos);
-        window.location.href = "/";
-      });
-    });
+    return login(id, contraseña)
+      .then(() => {
+        misPermisos().then((datosPermisos) => {
+          permisosGrupo.actualizar(datosPermisos.datos);
+        });
+      })
+      .then(() => navigate("/"));
   };
 
   return (
