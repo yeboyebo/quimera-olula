704c4d2f2a4653e8bd60e64e69bfed550e56789f
xavikortes <javiercortesveliz@gmail.com>
2026-01-14 12:55:47 +0100

    fix. problema creacion presupuestos

--- packages/contextos/src/ventas/presupuesto/diseño.ts
+++ packages/contextos/src/ventas/presupuesto/diseño.ts
@@ -64,9 +64,13 @@ export interface PresupuestoAPI {
 }
 
 export type NuevoPresupuesto = {
+  cliente: NuevoPresupuestoClienteRegistrado | NuevoPresupuestoClienteNoRegistrado;
+  empresa_id: string;
+}
+
+export type NuevoPresupuestoClienteRegistrado = {
   cliente_id: string;
   direccion_id: string;
-  empresa_id: string;
 };
 
 export type NuevoPresupuestoClienteNoRegistrado = {
