6bf6abe759736cc26eab2972566007c7023d2226
xavikortes <javiercortesveliz@gmail.com>
2026-01-20 17:19:09 +0100

    feat. adaptacion v2 crm.router_factory

--- packages/contextos/src/crm/router_factory.ts
+++ packages/contextos/src/crm/router_factory.ts
@@ -1,19 +1,19 @@
-import { MaestroConDetalleAccion } from "./accion/vistas/MaestroConDetalleAccion.tsx"
-import { MaestroConDetalleClienteCRM } from "./cliente/vistas/MaestroConDetalleCliente.tsx"
-import { MaestroConDetalleContacto } from "./contacto/vistas/MaestroConDetalleContacto.tsx"
-import { MaestroConDetalleEstadoOportunidad } from "./estadoOportunidadVenta/vistas/MaestroConDetalleEstadoOportunidad.tsx"
-import { MaestroConDetalleIncidencia } from "./incidencia/vistas/MaestroConDetalleIncidencia.tsx"
-import { MaestroLeads } from "./lead/Maestro/MaestroLeads.tsx"
-import { MaestroConDetalleOportunidadVenta } from "./oportunidadventa/vistas/MaestroConDetalleOportunidadVenta.tsx"
+import { MaestroAcciones } from "./accion/maestro/MaestroAcciones.tsx"
+import { MaestroClientes } from "./cliente/maestro/MaestroClientes.tsx"
+import { MaestroContactos } from "./contacto/maestro/MaestroContactos.tsx"
+import { MaestroEstadosOportunidad } from "./estadoOportunidadVenta/maestro/MaestroEstadosOportunidad.tsx"
+import { MaestroIncidencias } from "./incidencia/maestro/MaestroIncidencias.tsx"
+import { MaestroLeads } from "./lead/maestro/MaestroLeads.tsx"
+import { MaestroOportunidades } from "./oportunidadventa/maestro/MaestroOportunidadesVentas.tsx"
 
 export class RouterFactoryCrmOlula {
     static router = {
-        "crm/oportunidadventa": MaestroConDetalleOportunidadVenta,
-        "crm/estadooportunidadventa": MaestroConDetalleEstadoOportunidad,
-        "crm/cliente": MaestroConDetalleClienteCRM,
-        "crm/contacto": MaestroConDetalleContacto,
-        "crm/accion": MaestroConDetalleAccion,
+        "crm/oportunidadventa": MaestroOportunidades,
+        "crm/estadooportunidadventa": MaestroEstadosOportunidad,
+        "crm/cliente": MaestroClientes,
+        "crm/contacto": MaestroContactos,
+        "crm/accion": MaestroAcciones,
         "crm/lead": MaestroLeads,
-        "crm/incidencia": MaestroConDetalleIncidencia,
+        "crm/incidencia": MaestroIncidencias,
     }
 }
