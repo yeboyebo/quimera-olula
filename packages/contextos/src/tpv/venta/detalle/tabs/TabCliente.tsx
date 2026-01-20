562c12f1834c8bc0416b48c4cba336bb10092940
juanma.yeboyebo <juanma.yeboyebo@gmail.com>
2026-01-20 11:29:52 +0100

    fix. errores de ci en tpv/ventas

--- packages/contextos/src/tpv/venta/detalle/tabs/TabCliente.tsx
+++ packages/contextos/src/tpv/venta/detalle/tabs/TabCliente.tsx
@@ -8,6 +8,7 @@ import { Maquina, useMaquina } from "@olula/lib/useMaquina.ts";
 import { HookModelo } from "@olula/lib/useModelo.ts";
 import { useState } from "react";
 
+import { CambioClienteVenta } from "#/ventas/venta/diseño.ts";
 import { VentaTpv } from "../../diseño.ts";
 import { patchCambiarCliente } from "../../infraestructura.ts";
 import { CambioCliente } from "./CambioCliente.tsx";
@@ -19,58 +20,54 @@ interface TabClienteProps {
 }
 type Estado = "edicion" | "cambiando_cliente";
 
-export const TabCliente = ({
-  venta,
-  publicar = () => {},
-}: TabClienteProps) => {
-  
-    const [estado, setEstado] = useState<Estado>("edicion");
-    const { modelo, uiProps, editable } = venta;
+export const TabCliente = ({ venta, publicar = () => {} }: TabClienteProps) => {
+  const [estado, setEstado] = useState<Estado>("edicion");
+  const { modelo, uiProps, editable } = venta;
 
-    const maquina: Maquina<Estado> = {
-        edicion: {
-        CAMBIO_CLIENTE_INICIADO: "cambiando_cliente",
-        },
-        cambiando_cliente: {
-        CAMBIO_CLIENTE_CANCELADO: "edicion",
+  const maquina: Maquina<Estado> = {
+    edicion: {
+      CAMBIO_CLIENTE_INICIADO: "cambiando_cliente",
+    },
+    cambiando_cliente: {
+      CAMBIO_CLIENTE_CANCELADO: "edicion",
 
-        CAMBIO_CLIENTE_LISTO: async (payload: unknown) => {
-            const cambioCliente = payload as TipoCambioCliente;
-            await patchCambiarCliente(modelo.id, cambioCliente);
-            publicar("CLIENTE_FACTURA_CAMBIADO", modelo);
-            return "edicion" as Estado;
-        },
-        },
-    };
-    const emitir = useMaquina(maquina, estado, setEstado);
+      CAMBIO_CLIENTE_LISTO: async (payload: unknown) => {
+        const cambioCliente = payload as CambioClienteVenta;
+        await patchCambiarCliente(modelo.id, cambioCliente);
+        publicar("CLIENTE_FACTURA_CAMBIADO", modelo);
+        return "edicion" as Estado;
+      },
+    },
+  };
+  const emitir = useMaquina(maquina, estado, setEstado);
 
-    return (
-        <div className="TabCliente">
-        <quimera-formulario>
-            <Cliente {...uiProps("cliente_id", "nombre_cliente")} />
-            <QInput {...uiProps("id_fiscal")} label="ID Fiscal" />
-            <div id="cambiar_cliente" className="botones maestro-botones">
-            <QBoton
-                deshabilitado={!editable}
-                onClick={() => emitir("CAMBIO_CLIENTE_INICIADO")}
-            >
-                C
-            </QBoton>
-            </div>
-
-            <DirCliente
-            clienteId={modelo.cliente_id}
-            {...uiProps("direccion_id")}
-            />
-        </quimera-formulario>
-
-        <QModal
-            nombre="modal"
-            abierto={estado === "cambiando_cliente"}
-            onCerrar={() => emitir("CAMBIO_CLIENTE_CANCELADO")}
-        >
-            <CambioCliente publicar={emitir} />
-        </QModal>
+  return (
+    <div className="TabCliente">
+      <quimera-formulario>
+        <Cliente {...uiProps("cliente_id", "nombre_cliente")} />
+        <QInput {...uiProps("id_fiscal")} label="ID Fiscal" />
+        <div id="cambiar_cliente" className="botones maestro-botones">
+          <QBoton
+            deshabilitado={!editable}
+            onClick={() => emitir("CAMBIO_CLIENTE_INICIADO")}
+          >
+            C
+          </QBoton>
         </div>
-    );
+
+        <DirCliente
+          clienteId={modelo.cliente_id}
+          {...uiProps("direccion_id")}
+        />
+      </quimera-formulario>
+
+      <QModal
+        nombre="modal"
+        abierto={estado === "cambiando_cliente"}
+        onCerrar={() => emitir("CAMBIO_CLIENTE_CANCELADO")}
+      >
+        <CambioCliente publicar={emitir} />
+      </QModal>
+    </div>
+  );
 };
