08f277164668d2e3d38df9b3d9b7ce904e88c1df
juanma.yeboyebo <juanma.yeboyebo@gmail.com>
2026-03-03 13:34:38 +0100

    fix. error al arrancar

--- apps/elganso_stores/vite.config.ts
+++ apps/elganso_stores/vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+import { fileURLToPath, URL } from "node:url";
 import { mergeConfig } from "vite";
 import config from "../../vite.config.ts";
 
@@ -8,6 +9,7 @@ export default mergeConfig(config, {
     resolve: {
         alias: {
             '#': '@olula/ctx',
+            'use-db-state': fileURLToPath(new URL('./node_modules/use-db-state', import.meta.url)),
         }
     }
 });
