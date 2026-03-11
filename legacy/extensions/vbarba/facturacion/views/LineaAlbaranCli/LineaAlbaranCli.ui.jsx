64434c403830262e75ab322158e544c7a8aff555
Javier-Valero-yeboyebo <jvaola@gmail.com>
2026-03-11 12:08:52 +0100

    refact(Albaran): Añadido indicador de proveedor de artículo con selector cerrado o línea no editable.

--- legacy/extensions/vbarba/facturacion/views/LineaAlbaranCli/LineaAlbaranCli.ui.jsx
+++ legacy/extensions/vbarba/facturacion/views/LineaAlbaranCli/LineaAlbaranCli.ui.jsx
@@ -1,3 +1,4 @@
+import { Totales } from "@quimera-extension/base-area_clientes";
 import {
   Box,
   Button,
@@ -9,8 +10,7 @@ import {
   QSection,
   Typography,
 } from "@quimera/comps";
-import { Totales } from "@quimera-extension/base-area_clientes";
-import Quimera, { getSchemas, PropValidation, useStateValue, util } from "quimera";
+import Quimera, { getSchemas, useStateValue, util } from "quimera";
 import { useEffect } from "react";
 
 import { ProveedorArticulo, QArticuloVbarba } from "../../comps";
@@ -97,6 +97,7 @@ function LineaAlbaranCli({ callbackGuardada, disabled, lineaInicial, useStyles }
               saveDisabled={() => !schema.isValid(buffer)}
             >
               <Typography variant="h6">{buffer.descripcion}</Typography>
+              {buffer.nombreProveedor && <Typography variant="h6">Proveedor: {buffer.nombreProveedor}</Typography>}
             </QSection>
           </Grid>
 
