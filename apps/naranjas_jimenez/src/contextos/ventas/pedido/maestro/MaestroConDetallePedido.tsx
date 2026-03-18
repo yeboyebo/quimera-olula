e1f58b25457af45ba3cf4348eab0344208ef3461
José A. Fernández <aullasistemas@gmail.com>
2026-03-18 10:09:19 +0100

    feat(nrj) Tarjetas en pedido y lineas pedido

--- apps/naranjas_jimenez/src/contextos/ventas/pedido/maestro/MaestroConDetallePedido.tsx
+++ apps/naranjas_jimenez/src/contextos/ventas/pedido/maestro/MaestroConDetallePedido.tsx
@@ -1,20 +1,27 @@
 import { CrearPedido } from "#/ventas/pedido/crear/CrearPedido.tsx";
 import { DetallePedido } from "#/ventas/pedido/detalle/DetallePedido.tsx";
 import { getMaquina } from "#/ventas/pedido/maestro/maquina.ts";
+import { ColumnaEstadoTabla } from "#/comun/componentes/ColumnaEstadoTabla.tsx";
 import { QBoton } from "@olula/componentes/atomos/qboton.tsx";
 import { useMaquina } from "@olula/componentes/hook/useMaquina.js";
+import { QIcono } from "@olula/componentes/index.js";
 import { Listado } from "@olula/componentes/maestro/Listado.js";
 import { MaestroDetalle } from "@olula/componentes/maestro/MaestroDetalle.tsx";
 import { QModal } from "@olula/componentes/moleculas/qmodal.tsx";
+import { formatearFechaDate, formatearMoneda } from "@olula/lib/dominio.js";
 import { listaActivaEntidadesInicial } from "@olula/lib/ListaActivaEntidades.js";
 import { getUrlParams, useUrlParams } from "@olula/lib/url-params.js";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { PedidoNrj } from "../diseño.ts";
 import "./MaestroConDetallePedido.css";
 import { getMetaTablaPedidoNrj } from "./metatabla_pedido.tsx";
 
+type Layout = "TABLA" | "TARJETA";
+
 export const MaestroConDetallePedidoNrj = () => {
   const { id, criteria } = getUrlParams();
+
+  const [layout, setLayout] = useState<Layout>("TARJETA");
 
   const { ctx, emitir } = useMaquina(getMaquina, {
     estado: "INICIAL",
@@ -23,41 +30,17 @@ export const MaestroConDetallePedidoNrj = () => {
 
   useUrlParams(ctx.pedidos.activo, ctx.pedidos.criteria);
 
+  const cambiarLayout = useCallback(
+    () => setLayout(layout === "TARJETA" ? "TABLA" : "TARJETA"),
+    [layout, setLayout]
+  );
+
   useEffect(() => {
     emitir("recarga_de_pedidos_solicitada", ctx.pedidos.criteria);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const metaTablaPedido = getMetaTablaPedidoNrj();
-
-  // const metaTablaPedido = [
-  //   {
-  //     id: "estado",
-  //     cabecera: "",
-  //     render: (pedido: Pedido) => (
-  //       <ColumnaEstadoTabla
-  //         estados={{
-  //           aprobado: (
-  //             <QIcono
-  //               nombre={"circulo_relleno"}
-  //               tamaño="sm"
-  //               color="var(--color-deshabilitado-oscuro)"
-  //             />
-  //           ),
-  //           pendiente: (
-  //             <QIcono
-  //               nombre={"circulo_relleno"}
-  //               tamaño="sm"
-  //               color="var(--color-exito-oscuro)"
-  //             />
-  //           ),
-  //         }}
-  //         estadoActual={pedido.servido == "TOTAL" ? "aprobado" : "pendiente"}
-  //       />
-  //     ),
-  //   },
-  //   ...metaTablaBase,
-  // ] as MetaTabla<Pedido>;
 
   return (
     <div className="Pedido">
@@ -66,6 +49,12 @@ export const MaestroConDetallePedidoNrj = () => {
           <>
             <h2>Pedidos</h2>
             <div className="maestro-botones">
+              <QBoton
+                texto={
+                  layout === "TARJETA" ? "Cambiar a TABLA" : "Cambiar a TARJETA"
+                }
+                onClick={cambiarLayout}
+              />
               <QBoton onClick={() => emitir("crear_pedido_solicitado")}>
                 Nuevo Pedido
               </QBoton>
@@ -73,13 +62,17 @@ export const MaestroConDetallePedidoNrj = () => {
             <Listado<PedidoNrj>
               metaTabla={metaTablaPedido}
               criteria={ctx.pedidos.criteria}
-              modo={"tabla"}
+              modo={layout === "TARJETA" ? "tarjetas" : "tabla"}
+              tarjeta={TarjetaPedidoNrj}
               entidades={ctx.pedidos.lista as PedidoNrj[]}
               totalEntidades={ctx.pedidos.total}
               seleccionada={ctx.pedidos.activo}
               onSeleccion={(payload) => emitir("pedido_seleccionado", payload)}
               onCriteriaChanged={(payload) =>
                 emitir("criteria_cambiado", payload)
+              }
+              onSiguientePagina={(payload) =>
+                emitir("siguiente_pagina", payload)
               }
             />
           </>
@@ -95,6 +88,54 @@ export const MaestroConDetallePedidoNrj = () => {
       >
         <CrearPedido publicar={emitir} />
       </QModal>
+    </div>
+  );
+};
+
+const estadoCompleto = (
+  <QIcono
+    nombre={"circulo_relleno"}
+    tamaño="sm"
+    color="var(--color-exito-oscuro)"
+  />
+);
+
+const estadoPendiente = (
+  <QIcono
+    nombre={"circulo_relleno"}
+    tamaño="sm"
+    color="var(--color-error-oscuro)"
+  />
+);
+
+const estadoParcial = (
+  <QIcono
+    nombre={"circulo_relleno"}
+    tamaño="sm"
+    color="var(--color-advertencia-claro)"
+  />
+);
+
+const TarjetaPedidoNrj = (pedido: PedidoNrj) => {
+  return (
+    <div className="tarjeta-pedido" key={pedido.id}>
+      <div className="tarjeta-pedido-izquierda">
+        <ColumnaEstadoTabla
+          estados={{
+            completo: estadoCompleto,
+            pendiente: estadoPendiente,
+            parcial: estadoParcial,
+          }}
+          estadoActual={pedido.estado_envio_palets}
+        />
+        <div className="tarjeta-pedido-izquierda-textos">
+          <div>{`${pedido.codigo} - ${formatearFechaDate(pedido.fecha)}`}</div>
+          <div>{pedido.nombre_cliente}</div>
+        </div>
+      </div>
+      <div className="tarjeta-pedido-derecha">
+        {`${formatearMoneda(pedido.total, pedido.divisa_id)}`}
+      </div>
     </div>
   );
 };
