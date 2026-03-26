3241227dd31fd2ac6cda41dbd62c18e92476981f
Javier-Valero-yeboyebo <jvaola@gmail.com>
2026-03-26 09:25:57 +0100

    refact: espacios

--- legacy/extensions/vbarba/facturacion/views/LineaPresupuestoCli/LineaPresupuestoCli.ui.jsx
+++ legacy/extensions/vbarba/facturacion/views/LineaPresupuestoCli/LineaPresupuestoCli.ui.jsx
@@ -128,6 +128,7 @@ function LineaPresupuestoCli({ callbackGuardada, disabled, lineaInicial, useStyl
             </QSection>
           </Grid>
 
+
           <Grid item xs={12}>
             <QSection actionPrefix="totales" alwaysInactive>
               <Totales
