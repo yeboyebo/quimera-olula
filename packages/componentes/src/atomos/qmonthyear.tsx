8f4e09a54e4b6a3940a68e3ec0ae4b0a089a822e
Antonio Rodriguez <antonio@yeboyebo.es>
2026-07-06 14:25:31 +0200

    feat. filtro mes año. Parte manual

--- packages/componentes/src/atomos/qmonthyear.tsx
+++ packages/componentes/src/atomos/qmonthyear.tsx
@@ -1,5 +1,7 @@
 import "./_forminput.css";
 import { Etiqueta, FormFieldProps, Validacion } from "./_forminput.tsx";
+import { QBoton } from "./qboton.tsx";
+import { QDate } from "./qdate.tsx";
 
 export type QMonthYearProps = Omit<FormFieldProps, "label"> & {
   label?: string;
@@ -18,6 +20,50 @@ export const QMonthYear = ({
   valor,
   onChange,
 }: QMonthYearProps) => {
+  const manual = Array.isArray(valor);
+
+  const manejarChangeManual = () => {
+    onChange?.(
+      [undefined, undefined] as unknown as string,
+      undefined as unknown as React.ChangeEvent<HTMLInputElement>
+    );
+  };
+
+  if (manual) {
+    const [desde, hasta] = valor as unknown as [string, string];
+
+    const manejarChangeFecha = (
+      v: string,
+      e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+      const clave = e.target.name.split("__")[1];
+
+      const nuevoValor = [
+        clave === "desde" ? v : desde,
+        clave === "hasta" ? v : hasta,
+      ] as [string, string];
+
+      onChange?.(nuevoValor as unknown as string, e);
+    };
+
+    return (
+      <div className="interval">
+        <QDate
+          label={label + " desde"}
+          nombre={nombre + "__desde"}
+          valor={desde}
+          onChange={manejarChangeFecha}
+        />
+        <QDate
+          label={label + " hasta"}
+          nombre={nombre + "__hasta"}
+          valor={hasta}
+          onChange={manejarChangeFecha}
+        />
+      </div>
+    );
+  }
+
   const attrs = {
     nombre,
     erroneo,
@@ -33,19 +79,22 @@ export const QMonthYear = ({
   };
 
   return (
-    <quimera-date {...attrs}>
-      <label>
-        {label && <Etiqueta label={label} />}
-        <input
-          type="month"
-          name={nombre}
-          disabled={deshabilitado}
-          required={!opcional}
-          value={valor ?? ""}
-          onChange={manejarChange}
-        />
-        <Validacion textoValidacion={textoValidacion} />
-      </label>
-    </quimera-date>
+    <>
+      <quimera-date {...attrs}>
+        <label>
+          {label && <Etiqueta label={label} />}
+          <input
+            type="month"
+            name={nombre}
+            disabled={deshabilitado}
+            required={!opcional}
+            value={valor ?? ""}
+            onChange={manejarChange}
+          />
+          <Validacion textoValidacion={textoValidacion} />
+        </label>
+      </quimera-date>
+      <QBoton onClick={manejarChangeManual}>Manual</QBoton>
+    </>
   );
 };
